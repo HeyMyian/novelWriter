23b743b9bf9ea694387888fd213006d2c5b6ddae
HeyMyian <60368695+HeyMyian@users.noreply.github.com>
2025-08-22 20:16:20 +0200

    .

--- novelwriter/common.py
+++ novelwriter/common.py
@@ -445,6 +445,7 @@ def fontMatcher(font: QFont) -> QFont:
     default Qt font matching algorithm doesn't handle well changing
     application fonts at runtime.
     """
+    font.setStyleName(None)
     info = QFontInfo(font)
     if (famRequest := font.family()) != (famActual := info.family()):
         logger.warning("Font mismatch: Requested '%s', but got '%s'", famRequest, famActual)
